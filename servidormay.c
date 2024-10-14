1df958a993c74abc11439b122606239c54725658
Alba Costas <albacostasfernandez@gmail.com>
2024-10-14 09:59:11 +0200

    ACTUALICAXION SERVIRDORMAY

--- servidormay.c
+++ servidormay.c
@@ -1,8 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
-
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
@@ -13,53 +11,74 @@
 
 int main(int argc, char** argv) {
 
-    int puerto = 8080;
+    if(argc != 2) {
+        printf("Uso: %s <puerto>\n", argv[0]);
+        exit(1);
+    }
+
+    int puerto = atoi(argv[1]);  // Tomar el puerto desde los argumentos
     int servidor_fd, cliente_fd, n;
     struct sockaddr_in servidor_addr, cliente_addr;
     socklen_t cliente_len = sizeof(cliente_addr);
     char buffer[BUFFER];
-    
-    
-    
-    if((servidor_fd = socket(AF_INET, SOCK_STREAM, 0))<0){
-        printf("Error socket");
+
+    // Crear el socket
+    if((servidor_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+        perror("Error al crear el socket");
+        exit(1);
     }
+
     servidor_addr.sin_family = AF_INET;
     servidor_addr.sin_addr.s_addr = htons(INADDR_ANY); // Aceptar conexiones de cualquier IP
     servidor_addr.sin_port = htons(puerto);
 
-    if(bind(servidor_fd, (struct sockaddr *)&servidor_addr, sizeof(servidor_addr))<0){
-        printf("Error bind\n");
-        //close(servidor_fd);
+    // Enlazar el socket al puerto
+    if(bind(servidor_fd, (struct sockaddr *)&servidor_addr, sizeof(servidor_addr)) < 0){
+        perror("Error en el bind");
+        close(servidor_fd);
         exit(1);
     }
-    if(listen(servidor_fd,1)<0){
-        printf("Error listen\n");
-        //close(servidor_fd);
+
+    // Escuchar conexiones entrantes
+    if(listen(servidor_fd, 1) < 0){
+        perror("Error en el listen");
+        close(servidor_fd);
         exit(1);
     }
-    
-    while((n = recv(servidor_fd, buffer, BUFFER,0))>0){
 
-        if(n<0){
-            perror("Error al recibir.\n");
-        }else{
-            buffer[n] = '\0';
-            printf("Mensaje recibido: %s", buffer);
-            for(int i = 0;buffer[i] != '\0'; i++ ){
-                buffer[i]=toupper(buffer[i]);
-            }
-            if((cliente_fd = accept(servidor_fd, (struct sockaddr *)&cliente_addr, &cliente_len))<0){
-            printf("Error accept\n");
-            }
-            printf("Conexión aceptada de %s:%d\n", inet_ntoa(cliente_addr.sin_addr), ntohs(cliente_addr.sin_port));
-            
+    printf("Servidor escuchando en el puerto %d\n", puerto);
 
-        // parte c
-            send(cliente_fd,buffer, strlen(buffer),0);
+    while(1) {
+        // Aceptar una conexión
+        if((cliente_fd = accept(servidor_fd, (struct sockaddr *)&cliente_addr, &cliente_len)) < 0){
+            perror("Error al aceptar la conexión");
+            close(servidor_fd);
+            exit(1);
         }
+
+        printf("Conexión aceptada de %s:%d\n", inet_ntoa(cliente_addr.sin_addr), ntohs(cliente_addr.sin_port));
+
+        // Recibir datos del cliente
+        while((n = recv(cliente_fd, buffer, BUFFER, 0)) > 0) {
+            buffer[n] = '\0';  // Asegurar el final del string
+            printf("Mensaje recibido: %s", buffer);
+
+            // Convertir el mensaje a mayúsculas
+            for(int i = 0; buffer[i] != '\0'; i++) {
+                buffer[i] = toupper(buffer[i]);
+            }
+
+            // Enviar el mensaje de vuelta al cliente
+            send(cliente_fd, buffer, strlen(buffer), 0);
+        }
+
+        if(n < 0) {
+            perror("Error al recibir datos");
+        }
+
+        close(cliente_fd);  // Cerrar la conexión con el cliente
     }
 
-    //close(servidor_fd);
+    close(servidor_fd);
     return 0;
 }
